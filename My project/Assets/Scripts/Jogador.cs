cf6bacecbfb1f76ff432aefb0aa49ab534b2b77b
Pedro dos Santos Ferreira <pedro.progame558@gmail.com>
2026-04-16 21:16:17 -0300

    Tentativa falha de criar movimento para o jogador.

--- My project/Assets/Scripts/Jogador.cs
+++ My project/Assets/Scripts/Jogador.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jogador : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float speed = 5f;
+
+    private InputAction moveAction;
+    private Vector2 moveInput;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        InputSystem.actions.Disable();
+        playerInput.currentActionMap?.Enable();
+
+        moveAction = playerInput.actions?.FindAction("Move");
+
         
+        moveAction.performed += OnMovePerformed;
+        moveAction.canceled += OnMoveCanceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerMovement();
     }
+
+    void PlayerMovement()
+    {
+        // Atualiza velocidade do Rigidbody usando o input lido
+        Vector3 velocity = new Vector3(moveInput.x, rb.linearVelocity.y, moveInput.y) * speed;
+        rb.linearVelocity = velocity;
+    }
+
+    void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        moveInput = Vector2.zero;
+    }
+
+  
 }
